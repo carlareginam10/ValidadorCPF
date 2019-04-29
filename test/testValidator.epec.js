e7bd0fdcf3a44fad0e2bc243fe11f5318c62c047
carlareginam10 <carlareginam10@gmail.com>
2019-04-29 13:31:35 -0300

    refatoração da funçao validatorCPF utilizando o reduce

--- test/testValidator.epec.js
+++ test/testValidator.epec.js
@@ -2,86 +2,63 @@ let chai = require('chai');
 let expect = chai.expect;
 let validator = require('../lib/validator')
 
+describe('QUANDO O CPF É VÁLIDO', () => {
+    describe('e quando o resto é 10 ou 11 o dígito verificador deve ser zero', () => {
+        it('deve retornar true',()=>{
+            expect(validator.validatorCPF('36472172200')).to.equal(true);                
+        });       
+    });
+   
+    describe('e os números são diferentes', () => {
+        it('deve retornar true', ()=>{
+             expect(validator.validatorCPF('52998224725')).to.equal(true);
+        });       
+    });
+});   
 
-describe('validator lib', () => {
-    describe("validatorCPF",()=>{
-        it('Validar CPF', ()=>{
-            expect(validator.validatorCPF('03090575663')).to.equal(true);
+describe('QUANDO O CPF É INVÁLIDO', () => {
+    describe('e os números são todos iguais a 0', () => {
+        it('deve retornar false',()=>{
+            expect(validator.validatorCPF('00000000000')).to.equal(false);
         });
-        it('Validar CPF 1', ()=>{
+    });
+    describe('e os números são diferentes', () => {
+        it('deve retornar false', ()=>{
             expect(validator.validatorCPF('03090575662')).to.equal(false);
         });
-        it('Validar CPF 2',()=>{
-            expect(validator.validatorCPF('00000000000')).to.equal(false);
-            
-        })
-        it('Validar CPF 2',()=>{
-            expect(validator.validatorCPF('00000000000')).to.equal(false);
-            
-        })
-        // it('Validar CPF 3',()=>{
-        //     expect(validator.validatorCPF('04864713901')).to.equal(true);
-            
-        // })
-        // it('Validar CPF 4',()=>{
-        //     expect(validator.validatorCPF('77777777777')).to.equal(false);
-            
-        // })
-        // it('Validar CPF 5',()=>{
-        //     expect(validator.validatorCPF('12345678910')).to.equal(false);
-            
-        // })
-        it('Validar CPF 6',()=>{
-            expect(validator.validatorCPF('1234')).to.equal(false);
-            
-        })
-        // it('Validar CPF 7', ()=>{
-        //     expect(validator.validatorCPF('03090575622')).to.equal(false);
-        //  });
-        it('Validar CPF 7', ()=>{
-            expect(validator.validatorCPF('03090575623')).to.equal(false);
-        });
-        // it('Validar CPF 8', ()=>{
-        //     expect(validator.validatorCPF('04998264931')).to.equal(false);
-        // });
-        it('Validar CPF 9', ()=>{
-            expect(validator.validatorCPF('36472172200')).to.equal(true);
-        });
-        it('Validar CPF 9', ()=>{
-            expect(validator.validatorCPF('true')).to.equal(false);
-        });
-        it('Validar CPF 9', ()=>{
-            expect(validator.validatorCPF(true)).to.equal(false);
-        });
-        it('Validar CPF 9', ()=>{
-            expect(validator.validatorCPF(false)).to.equal(false);
-        });
-        
-        
     });
-
-
-
+    describe('e tem menos que 11 números', () => {
+        it('deve retornar false', ()=>{
+            expect(validator.validatorCPF('1234')).to.equal(false);
+        });                                      
+    });
+    describe('e o ultimo dígito verificador não confere', () => {
+         it('deve retornar false', ()=>{
+            expect(validator.validatorCPF('03090575662')).to.equal(false);
+        });                           
+    });
+    describe('e todos os número são iguais a 7', () => {
+        it('deve retornar false', ()=>{
+            expect(validator.validatorCPF('77777777777')).to.equal(false);
+        });               
+    });
+    describe('e quando é uma string', () => {
+        it('deve retornar false', ()=>{
+            expect(validator.validatorCPF('true')).to.equal(false);
+        });                               
+    });
+    describe('e quando o primeiro dígito verificador não confere', () => {
+        it('deve retornar false', ()=>{
+            expect(validator.validatorCPF('03090575623')).to.equal(false);
+        });                               
+    });
+    describe('e o número é maior que 11', () => {
+        it('deve retornar false', ()=>{
+            expect(validator.validatorCPF('03090575623222')).to.equal(false);
+        });                               
+    });
 });
 
-// let chai = require('chai');
-// let expect = chai.expect;
-// let validator = require('../lib/validator2')
-
-
-// describe('validator lib', () => {
-//     describe("validate",()=>{
-//         it('Validar CPF', ()=>{
-//             expect(validator.validate('03090575663')).to.equal(true);
-//         });
-//         // it('test2',()=>{
-//         //     expect(1).to.equal(1);
-//         // })
-        
-//     });
-
-
-// });
 
 
 // https://github.com/guilhermehn/validar-cpf
